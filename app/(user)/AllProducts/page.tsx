74c51950c1c6e06fa0d45397894e2d37dfc0ec8d
Wenceslaus Obimka <obimkasomto@gmail.com>
2024-03-27 13:25:09 +0100

    enhance mobile responsiveness

--- app/(user)/AllProducts/page.tsx
+++ app/(user)/AllProducts/page.tsx
@@ -38,7 +38,7 @@ const AllProducts = () => {
                 </Link> */}
             </div>
 
-            <div className='mt-6 grid grid-cols-1 gap-x-6 gap-y-10 sm:grid-cols-2 lg:grid-cols-4 xl:gap-x-8'>
+            <div className='mt-6 grid md:grid-cols-3 gap-x-6 gap-y-10 grid-cols-2 lg:grid-cols-4 xl:gap-x-8'>
                 {products.map((product) =>(
                     <div key={product._id} className='group relative'>
                         <div className='aspect-square w-full overflow-hidden rounded-md bg-gray-200 group-hover:opacity-75 lg:h-80'>
