7a7021bf945aee7e314cf5f689a1daad73096ba2
Wenceslaus Obimka <obimkasomto@gmail.com>
2024-04-06 17:30:47 +0100

    develop footer

--- app/(user)/layout.tsx
+++ app/(user)/layout.tsx
@@ -5,6 +5,7 @@ import Navbar from "./components/Navbar";
 import { CartProvider } from "use-shopping-cart";
 import CartProviders from "./components/Providers";
 import ShoppingCartModal from "./components/ShoppingCartModal";
+import Footer from "./components/Footer";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -25,6 +26,7 @@ export default function RootLayout({
           <Navbar />
           <ShoppingCartModal />
           {children}
+          <Footer />
         </CartProviders>
       </body>
     </html>
